df9934d856696f70c9f9407bcbc3e53f6b991a70
progmamun <mamun333089@gmail.com>
2023-01-21 12:57:01 +0600

    server-done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const moment = require("moment");
 const port = process.env.PORT || 5000;
 
 const cors = require("cors");
+const moment = require("moment");
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +20,36 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
+    const db = client.db("techBlog");
+    const blogCollection = db.collection("blogs");
+
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogCollection.find({});
+      const blog = await cursor.toArray();
+
+      res.send({ status: true, data: blog });
+    });
+
+    app.post("/blog", async (req, res) => {
+      const blog = req.body;
+      const postedAt = moment();
+      const result = await blogCollection.insertOne({ ...blog, postedAt });
+
+      res.send(result);
+    });
+
+    app.patch("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const result = await blogCollection.updateOne({ _id: ObjectId(id) });
+      res.send(result);
+    });
+
+    app.delete("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await blogCollection.deleteOne({ _id: ObjectId(id) });
+      res.send(result);
+    });
   } finally {
   }
 };
